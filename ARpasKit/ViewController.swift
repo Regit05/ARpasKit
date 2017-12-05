2cb0651076cfe7338e27fdc341827bbc9b8687d1
admin <celine.meme@eemi.com>
2017-12-05 11:58:10 +0100

    Apparition Carré Blanc sur soi

--- ARpasKit/ViewController.swift
+++ ARpasKit/ViewController.swift
@@ -22,14 +22,33 @@ class ViewController: UIViewController {
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
+        
+        let cubeNode2 = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        cubeNode2.position = SCNVector3(0,0,-0.2)
+        sceneView.scene.rootNode.addChildNode(cubeNode2)
+        
+        /*let session = ARSession()
+        
+        guard let cameraTransform = session.currentFrame?.camera.transform, let focusSquarePosition = focusSquare.lastPosition else {
+            statusViewController.showMessage("Cannot place objecct")
+            return
+        }
+        
+        virtualObjectInteraction.selectedObject = cubeNode2
+        cubeNode2.simdConvertPosition(focusSquarePosition, relativeTo : cameraTransform, smoothMovement: false)
+        
+        updateQueue.async {
+            self.sceneView.scene.rootNode.addChildNode(cubeNode2)
+        }*/
+        
     }
 
     
-    @IBAction func button(_ sender: Any) {
+    /*@IBAction func button(_ sender: Any) {
         let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
         cubeNode.position = SCNVector3(0,0,-0.2)
         sceneView.scene.rootNode.addChildNode(cubeNode)
-    }
+    }*/
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
