4653e2d58f4c6903ae83734c06a447cf4820a826
admin <celine.meme@eemi.com>
2017-12-05 10:20:58 +0100

    First commit

--- ARpasKit/ViewController.swift
+++ ARpasKit/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
     }
 
+    
+    @IBAction func button(_ sender: Any) {
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        cubeNode.position = SCNVector3(0,0,-0.2)
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
